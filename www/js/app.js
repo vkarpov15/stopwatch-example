221f280761d3f61a5dc1d15e92731c5507632d29
Valeri Karpov <valkar207@gmail.com>
2015-10-01 17:22:14 -0400

    part 3 step 4; make functional app

--- www/js/app.js
+++ www/js/app.js
@@ -64,4 +64,28 @@ angular.module('starter',
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
 
+})
+
+.config(function($httpProvider) {
+  $httpProvider.interceptors.push(function() {
+    return {
+      request: function(req) {
+        // Transform **all** $http calls so that requests that go to `/`
+        // instead go to a different origin, in this case localhost:3000
+        if (req.url.charAt(0) === '/') {
+          req.url = 'http://localhost:3000' + req.url;
+          // and make sure to send cookies too
+          req.withCredentials = true;
+        }
+
+        return req;
+      }
+    };
+  });
+})
+
+.run(function($rootScope) {
+  $rootScope.$on('NEW_TIME_SAVED', function(ev, time) {
+    $rootScope.$broadcast('NEW_TIME', time);
+  });
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,28 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+.factory('$facebookLogin', function($user) {
+  return function() {
+    var url = 'http://localhost:3000/auth/facebook';
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
+    var ref = window.open(url, '_blank', 'location=no');
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    // For Cordova
+    if (window.cordova) {
+      ref.addEventListener('loadstop', function(ev) {
+        if (ev.url.indexOf('/auth/facebook/callback') !== -1) {
+          ref.close();
+          $user.load();
         }
-      }
-      return null;
+      });
+    } else {
+      // For `ionic serve --lab`. Wait for the user to close the window
+      // and, when they do, check the server to see if they're now logged in.
+      var interval = setInterval(function() {
+        if (ref.closed) {
+          $user.load();
+          clearInterval(interval);
+        }
+      }, 100);
     }
   };
 });
